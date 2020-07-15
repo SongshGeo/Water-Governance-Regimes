6d59bbe54399e25b29124b4af626b1c6b2143c26
SongshGeo <songshgeo@gmail.com>
2020-07-15 22:56:33 +0800

    做完了利用陷阱的实验

--- scripts/formula_script.R
+++ scripts/formula_script.R
@@ -12,23 +12,22 @@ myd$Basin <- as.character(myd$Basin)
 dataprep.out <-
   dataprep(
            foo = myd,
-           predictors    = c("Irrigated.area..Total", "Industrial.gross.value.added..GVA...Total", "Urban.population", "Service.GVA", "Rural.population", "Livestock.population"),
+           predictors    = c("Irrigated.area..Total", "Irrigated.area..Rice", "Irrigated.area..Wheat", "Irrigated.area..Maize", "Irrigated.area..Vegetables.and.fruits", "Irrigated.area..Others", "Industrial.gross.value.added..GVA...Total", "Industrial.gross.value.added..GVA...Textile", "Industrial.gross.value.added..GVA...Papermaking", "Industrial.gross.value.added..GVA...Petrochemicals", "Industrial.gross.value.added..GVA...Metallurgy", "Industrial.gross.value.added..GVA...Mining", "Industrial.gross.value.added..GVA...Food", "Industrial.gross.value.added..GVA...Cements", "Industrial.gross.value.added..GVA...Machinery", "Industrial.gross.value.added..GVA...Electronics", "Industrial.gross.value.added..GVA...Thermal.electrivity", "Industrial.gross.value.added..GVA...Others", "Urban.population", "Service.GVA", "Rural.population", "Livestock.population"),
            dependent     = "Total.water.use",
            unit.variable = "Index",
            time.variable = "Year",
            treatment.identifier = 3,
            controls.identifier = c(0, 1, 2, 4, 5, 6, 7, 8),
-           time.predictors.prior = 1965:1994,
+           time.predictors.prior = 1978:1994,
            time.optimize.ssr = 1995:2013,
            unit.names.variable = "Basin",
-           time.plot = 1965:2013
+           time.plot = 1978:2013
          )
 
 # fit training model
 synth.out <- 
   synth(
-        data.prep.obj=dataprep.out,
-        Margin.ipop=.005,Sigf.ipop=7,Bound.ipop=6
+        data.prep.obj=dataprep.out
         )
 
 
